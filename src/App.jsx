2cfe7e063e7bfd81e2c117db601a382312036b71
Abdulfetah Suudi <suudiabdulfetah@gmail.com>
2024-06-28 23:51:38 +0300

    Initialize the project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{backgroundImage: "url('898403.jpg')"}} id="window" className="w-screen h-screen">
+      {/* <p className="">hello</p> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Theme appearance="dark">
+      <App />
+    </Theme>
+  </React.StrictMode>
+);
