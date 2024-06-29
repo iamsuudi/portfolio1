734d9a63ed6a1f93b891b80b984daaa411f08944
Abdulfetah Suudi <suudiabdulfetah@gmail.com>
2024-06-29 09:41:02 +0300

    feat: format Date

--- src/components/window/Heading.jsx
+++ src/components/window/Heading.jsx
@@ -3,6 +3,22 @@ import NightIcon from "../../assets/NightIcon";
 import WifiIcon from "../../assets/WifiIcon";
 import SoundIcon from "../../assets/SoundIcon";
 import BatteryIcon from "../../assets/BatteryIcon";
+import { useEffect } from "react";
+import { format } from "date-fns";
+import { useState } from "react";
+
+function DateFormatted() {
+  const [time, setTime] = useState(new Date());
+  
+  useEffect(() => {
+    const checker = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(checker);
+  }, [time]);
+  
+  return <span>{format(time, "MMM d HH:mm")}</span>;
+}
 
 export default function HeaderBar() {
   return (
@@ -13,7 +29,9 @@ export default function HeaderBar() {
         </button>
       </div>
       <div className="px-4 py-1 rounded-2xl hover:bg-white/10">
-        <button className="text-sm ">{new Date().toDateString()}</button>
+        <button className="text-sm ">
+          <DateFormatted />
+        </button>
       </div>
       <div className="px-4 py-1 rounded-2xl hover:bg-white/10">
         <button className="flex items-center gap-3 ">
