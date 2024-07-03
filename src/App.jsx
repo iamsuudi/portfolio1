66e32e52e2529fe15d37aaf65e1efe10011dfd31
Abdulfetah Suudi <suudiabdulfetah@gmail.com>
2024-07-03 16:14:50 +0300

    feat: Add loading page and update unSpoorted screen

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,42 @@ import Window from "./components/window/Window";
 import { useState } from "react";
 import { Avatar } from "@radix-ui/themes";
 
+function UnSupportedDevice() {
+	return (
+		<div className="w-[100dvw] h-[100dvh] flex justify-center items-center gap-10 flex-col p-5">
+			<Avatar
+				src="gift.png"
+				fallback="Gift Image"
+				className="animate-bounce size-80"
+			/>
+			<span className={`text-sm font-bold text-center text-amber-600`}>
+				Use Laptop or Desktop to open me
+			</span>
+		</div>
+	);
+}
+
+function LoadingScreen() {
+	return (
+		<div className="w-[100dvw] h-[100dvh] flex justify-center items-center gap-10 flex-col p-5">
+			<Avatar
+				src="ubuntu.png"
+				fallback="Ubuntu Logo"
+				size={"9"}
+				className=""
+			/>
+			<span
+				className={`text-xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-gray-700 via-gray-50 to-gray-700 bg-300% animate-gradient`}
+			>
+				Loading Ubuntu...
+			</span>
+		</div>
+	);
+}
+
 function App() {
 	const [viewPortIsLarge, setViewPortIsLarge] = useState(true);
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
 		const checkViewPortSize = () => {
@@ -15,31 +49,25 @@ function App() {
 			else setViewPortIsLarge(false);
 		};
 
+		const timer = setTimeout(() => {
+			setIsLoading(false);
+		}, 5000);
+
 		checkViewPortSize();
 
 		window.addEventListener("resize", checkViewPortSize);
 
-		return () => window.removeEventListener("resize", checkViewPortSize);
+		return () => {
+			window.removeEventListener("resize", checkViewPortSize);
+			clearTimeout(timer);
+		};
 	}, []);
 
-	if (!viewPortIsLarge)
-		return (
-			<div className="w-[100dvw] h-[100dvh] flex justify-center items-center gap-10 flex-col p-5">
-				<Avatar
-					src="ubuntu.png"
-					fallback="Ubuntu Logo"
-					size={"9"}
-					className="animate-bounce"
-				/>
-				<span
-					className={`text-3xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-orange-500`}
-				>
-					Use Laptop or Desktop!
-				</span>
-			</div>
-		);
+	if (isLoading) return <LoadingScreen />;
 
-	return <Window>{/* <h1>heading</h1> */}</Window>;
+	if (viewPortIsLarge) return <Window>{/* <h1>heading</h1> */}</Window>;
+
+	return <UnSupportedDevice />;
 }
 
 export default App;
